516369a7217f3b6ecc1e54705bf2b8cb04546537
amjed-ali-k <amjedmgm@gmail.com>
2023-08-11 22:45:49 +0530

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -33,6 +34,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-3DZN7M50RE" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-3DZN7M50RE');
+        `}
+      </Script>
       <body className={inter.className}>
         <div>
           <Toaster />
