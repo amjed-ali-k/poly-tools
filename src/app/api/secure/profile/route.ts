e91af7c29ece4844a6c5db9c153cdad6c508bcfc
amjed-ali-k <amjedmgm@gmail.com>
2023-09-10 15:18:12 +0530

    fix: add profile apis

---
+++ src/app/api/secure/profile/route.ts
@@ -0,0 +1,105 @@
+import { z } from "zod";
+import { NextRequest, NextResponse } from "next/server";
+import { getServerAuthSession } from "@/server/auth/server";
+import { prisma } from "@/server/db/prisma";
+
+const schema = z.object({
+  phone: z
+    .string()
+    .nonempty()
+    .regex(/^[0-9]{10}$/),
+  bio: z.string().optional(),
+  name: z.string().nonempty(),
+  college: z.string().nonempty(),
+  image: z.string().url().optional(),
+  designation: z.string().nonempty(),
+  links: z
+    .object({
+      instagram: z.string().url(),
+      linkedin: z.string().url(),
+      github: z.string().url(),
+      facebook: z.string().url(),
+      twitter: z.string().url(),
+      threads: z.string().url(),
+      telegram: z.string().url(),
+    })
+    .optional(),
+});
+
+export async function PUT(request: NextRequest) {
+  // get session
+
+  const body = schema.safeParse(await request.json());
+
+  if (!body.success) {
+    const { errors } = body.error;
+    return NextResponse.json(
+      { message: "Invalid request", errors },
+      { status: 400 }
+    );
+  }
+  const session = await getServerAuthSession();
+
+  // if no session, throw unauthenticated response
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ message: "Unauthenticated" }, { status: 401 });
+  }
+
+  // save data to db
+  const links = body.data.links;
+  const res = await prisma.user.update({
+    where: {
+      id: session.user.id,
+    },
+    data: {
+      phone: body.data.phone,
+      bio: body.data.bio,
+      name: body.data.name,
+      collegeId: body.data.college,
+      image: body.data.image,
+      designation: body.data.designation,
+      userLinks: links && {
+        create: Object.keys(links)
+          .map((key) => {
+            const name = key as keyof typeof links;
+            const url = links[name] as string;
+            if (url)
+              return {
+                name,
+                url,
+              };
+          })
+          .filter((e) => e !== undefined) as {
+          name: string;
+          url: string;
+        }[],
+      },
+    },
+    include: {
+      userLinks: true,
+    },
+  });
+
+  return NextResponse.json({ success: true, now: Date.now(), res });
+}
+
+export async function GET(request: NextRequest) {
+  const session = await getServerAuthSession();
+
+  // if no session, throw unauthenticated response
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ message: "Unauthenticated" }, { status: 401 });
+  }
+
+  const res = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+    include: {
+      userLinks: true,
+      college: true,
+    },
+  });
+
+  return NextResponse.json({ ...res });
+}
--- src/app/dashboard/profile/_components/form.tsx
+++ src/app/dashboard/profile/_components/form.tsx
@@ -18,39 +18,45 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Textarea } from "@/components/ui/textarea";
 
 import { useToast } from "@/components/ui/use-toast";
+import { useSession } from "@/lib/auth";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CaretSortIcon } from "@radix-ui/react-icons";
+import axios from "axios";
 import { CheckIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
-const fomrSchema = z.object({
+const formSchema = z.object({
   phone: z
     .string()
     .nonempty()
-    .regex(/^[0-9]{10}$/),
-  bio: z.string(),
-  name: z.string().nonempty(),
+    .regex(/^[0-9]{10}$/, "Enter a valid phone number."),
+  bio: z.string().optional(),
+  name: z.string().min(5, "Minimum 5 characters required").nonempty("Required"),
   college: z.string().nonempty(),
-  image: z.string().url(),
-
-  links: z.object({
-    instagram: z.string().url(),
-    linkedin: z.string().url(),
-    github: z.string().url(),
-    facebook: z.string().url(),
-    twitter: z.string().url(),
-    threads: z.string().url(),
-    telegram: z.string().url(),
-  }),
+  image: z.string().url().optional(),
+  designation: z.string().nonempty(),
+  links: z
+    .object({
+      instagram: z.string().url(),
+      linkedin: z.string().url(),
+      github: z.string().url(),
+      facebook: z.string().url(),
+      twitter: z.string().url(),
+      threads: z.string().url(),
+      telegram: z.string().url(),
+    })
+    .optional(),
 });
 
 export function ProfileForm({
@@ -65,90 +71,175 @@ export function ProfileForm({
   }[];
 }) {
   const { toast } = useToast();
-
-  const form = useForm<z.infer<typeof fomrSchema>>({
-    resolver: zodResolver(fomrSchema),
+  const { data } = useSession();
+  console.log(data);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {},
   });
 
-  function onSubmit(data: z.infer<typeof fomrSchema>) {
-    toast({
-      title: "Profile Updated",
-      description: "Your profile has been updated successfully.",
-    });
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    axios
+      .put("/api/secure/profile", data)
+      .then((res) => {
+        console.log(res.data);
+        toast({
+          title: "Profile Updated",
+          description: "Your profile has been updated successfully.",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        toast({
+          title: "Profile Update Failed",
+          description: "Your profile could not be updated.",
+          variant: "destructive",
+        });
+      });
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <section className="grid  md:grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem aria-required>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Your name" {...field} />
+                </FormControl>
+                <FormDescription>Enter you full name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="designation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Designation</FormLabel>
+                <FormControl>
+                  <Input placeholder="Lecturer" {...field} />
+                </FormControl>
+                <FormDescription>Your current Designation</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="college"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel className="my-1">College</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "min-w-[340px] justify-between truncate whitespace-nowrap flex-nowrap overflow-hidden",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value
+                          ? collegeList.find(
+                              (language) => language.code === field.value
+                            )?.name
+                          : "Select College"}
+                        <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[600px] p-0" align="start">
+                    <Command>
+                      <CommandInput
+                        placeholder="Search colleges..."
+                        className="h-9"
+                      />
+                      <CommandList>
+                        <CommandEmpty>No college found.</CommandEmpty>
+                        <CommandGroup>
+                          {collegeList.map((college) => (
+                            <CommandItem
+                              value={college.name}
+                              key={college.code}
+                              onSelect={() => {
+                                form.setValue("college", college.code);
+                              }}
+                            >
+                              {college.name}
+                              <CheckIcon
+                                className={cn(
+                                  "ml-auto h-4 w-4",
+                                  college.code === field.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Select the college you are currently working in.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone number</FormLabel>
+                <FormControl>
+                  <Input placeholder="+91 97xxxx" {...field} />
+                </FormControl>
+                <FormDescription>Enter you phone number.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </section>
+
         <FormField
           control={form.control}
-          name="college"
+          name="bio"
           render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>College</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant="outline"
-                      role="combobox"
-                      className={cn(
-                        "w-[400px] justify-between truncate whitespace-nowrap flex-nowrap overflow-hidden",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value
-                        ? collegeList.find(
-                            (language) => language.code === field.value
-                          )?.name
-                        : "Select College"}
-                      <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-[600px] p-0" align="start">
-                  <Command>
-                    <CommandInput
-                      placeholder="Search colleges..."
-                      className="h-9"
-                    />
-                    <CommandList>
-                      <CommandEmpty>No college found.</CommandEmpty>
-                      <CommandGroup>
-                        {collegeList.map((college) => (
-                          <CommandItem
-                            value={college.name}
-                            key={college.code}
-                            onSelect={() => {
-                              form.setValue("college", college.code);
-                            }}
-                          >
-                            {college.name}
-                            <CheckIcon
-                              className={cn(
-                                "ml-auto h-4 w-4",
-                                college.code === field.value
-                                  ? "opacity-100"
-                                  : "opacity-0"
-                              )}
-                            />
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Few words about you (optional)"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
               <FormDescription>
-                Select the college you are currently working in.
+                This is optional. You can tell us a little bit about yourself.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button role="button" type="submit">
-          Submit
+        <Button
+          role="button"
+          disabled={form.formState.isSubmitting}
+          type="submit"
+        >
+          Updated profile
         </Button>
       </form>
     </Form>
