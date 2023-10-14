7038665df245cc7a8cf1935557b190ed2d481377
amjed-ali-k <amjedmgm@gmail.com>
2023-10-14 22:59:08 +0530

    fix: key

--- src/components/auth/server.ts
+++ src/components/auth/server.ts
@@ -14,18 +14,18 @@ export const fetchUserId = async (hanko: string) => {
 };
 
 export const getUserId = async (req: NextRequest) => {
-  const userId = req.headers.get("x-user-id");
-  return userId;
-  // const hanko = req.cookies.get("hanko")?.value ?? "";
-  // if (!hanko) {
-  //   throw new Error("No Hanko cookie found");
-  // }
-  // const userId = await fetchUserId(hanko);
-  // // validate userId is uuid using zod
-  // const schema = z.string().uuid();
-  // const validatedUserId = schema.safeParse(userId);
-  // if (!validatedUserId.success) {
-  //   throw new Error("Invalid user id");
-  // }
-  // return validatedUserId.data;
+  // const userId = req.headers.get("x-user-id");
+  // return userId;
+  const hanko = req.cookies.get("hanko")?.value ?? "";
+  if (!hanko) {
+    return null;
+  }
+  const userId = await fetchUserId(hanko);
+  // validate userId is uuid using zod
+  const schema = z.string().uuid();
+  const validatedUserId = schema.safeParse(userId);
+  if (!validatedUserId.success) {
+    throw new Error("Invalid user id");
+  }
+  return validatedUserId.data;
 };
